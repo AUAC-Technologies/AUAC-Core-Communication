b36a04058e6ad35c1812ee8385ee4600fa63079c
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-09-30 09:19:03 +0000

    changes and additions to source files

--- auac-comm-module-link/comm-module-link.h
+++ auac-comm-module-link/comm-module-link.h
@@ -1,6 +1,15 @@
 #ifndef COMM_MODULE_LINK
 #define COMM_MODULE_LINK
 
+#include"../AUAC_TYPEDEFS/AUAC_TYPES.h"
+
+AUAC_BASE_STRING bluetoothTrasnmit(AUAC_BASE_STRING data){
+
+}
+
+AUAC_BASE_STRING bluetoothReceive(){
+
+}
 
 
 #endif //COMM_MODULE_LINK
